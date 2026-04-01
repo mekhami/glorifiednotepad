4584a763a1012d57bea4ca49f234723ccbf37fce
Lawrence Vanderpool <vanderpool.lawrence@heb.com>
2026-04-01 12:02:37 -0500

    feat: add downloads to static_paths

--- lib/indie_web.ex
+++ lib/indie_web.ex
@@ -17,7 +17,7 @@ defmodule IndieWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico favicon.svg robots.txt)
+  def static_paths, do: ~w(assets downloads fonts images favicon.ico favicon.svg robots.txt)
 
   def router do
     quote do
